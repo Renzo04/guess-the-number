7348c84585e9637c95760bd22d999fbb7c3cf2b3
Renzo Manganiello <renzo@fmanganiello.com.ar>
2018-07-27 20:16:10 -0300

    Add file that holds all the constants of the project

---
+++ gtn/constants.py
@@ -0,0 +1,12 @@
+# Range used to select a number in each game.
+NUMBERS_RANGE = range(1, 101)
+
+# Games' identifiers
+COMPUTER_GAME = 'c'
+HUMAN_GAME = 'h'
+
+# Allowed commands used in the Human Game
+LOWER = '-'
+HIGHER = '+'
+MATCH = '='
+VALID_HUMAN_GAME_INPUTS = [LOWER, HIGHER, MATCH]
